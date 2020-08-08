d77f872ad45f3228211a21b615babdcf024e7c4b
Grace Guo <grace.ruisi.guo@gmail.com>
2020-08-07 21:36:25 -0700

    fixed some dark mode/ light mode issues and tried to fix email issue but haven't deployed to test yet

--- MyAllergy/ViewController3.swift
+++ MyAllergy/ViewController3.swift
@@ -7,43 +7,43 @@
 
 import UIKit
 import MessageUI
+import Foundation
 
 class ViewController3: UIViewController, UITextViewDelegate, MFMailComposeViewControllerDelegate {
     
-    private func configureTextView() {
-        feedbackView.delegate = self
+    var feedback = ""
+    
+    func configuredMailComposeViewController() -> MFMailComposeViewController {
+        let mailComposerVC = MFMailComposeViewController()
+        mailComposerVC.mailComposeDelegate = self // Extremely important to set the --mailComposeDelegate-- property, NOT the --delegate-- property
+
+        mailComposerVC.setToRecipients(["allergydiagnosisforall@gmail.com"])
+        mailComposerVC.setSubject("Feedback for MyAllergy App")
+        mailComposerVC.setMessageBody(feedback, isHTML: false)
+
+        return mailComposerVC
     }
+
+    func showSendMailErrorAlert() {
+        let sendMailErrorAlert = UIAlertView(title: "Could Not Send Email", message: "Unfortunately your request could not be completed. Please try again at another time and we're very sorry for any inconvenience.", delegate: self, cancelButtonTitle: "OK")
+        sendMailErrorAlert.show()
+    }
+
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true, completion: nil)
+        }
 
     @IBOutlet weak var feedbackView: UITextView!
     
     @IBAction func submitTapped(_ sender: UIButton) {
-        let feedback = feedbackView.text!
+        feedback = feedbackView.text!
         
-        func sendEmail() {
-            let composeVC = MFMailComposeViewController()
-            composeVC.mailComposeDelegate = self
-            composeVC.setToRecipients(["allergydiagnosisforall@gmail.com"])
-            composeVC.setSubject("Feedback for MyAllergy App")
-            composeVC.setMessageBody(feedback, isHTML: false)
-            
-            self.present(composeVC, animated: true, completion: nil)
-        }
-
-        func mailComposeController(controller: MFMailComposeViewController,
-                               didFinishWithResult result: MFMailComposeResult, error: NSError?) {
-            controller.dismiss(animated: true, completion: nil)
-        }
-        
-        if !MFMailComposeViewController.canSendMail() {
-            let alert = UIAlertController(title: "Error:", message: "Unfortunately your request could not be completed. Please try again at another time and we're very sorry for any inconvenience.", preferredStyle: .alert)
-
-            alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
-
-            self.present(alert, animated: true)
-        }
-        else {
-            sendEmail()
-        }
+        let mailComposeViewController = configuredMailComposeViewController()
+            if MFMailComposeViewController.canSendMail() {
+                self.present(mailComposeViewController, animated: true, completion: nil)
+            } else {
+                self.showSendMailErrorAlert()
+            }
     }
     
     override func viewDidLoad() {
